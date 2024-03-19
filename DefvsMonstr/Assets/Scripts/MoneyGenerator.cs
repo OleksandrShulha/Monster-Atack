4d08df9cef98f35ccfc86890b82d8666e1b1605f
OleksandrShulha <106068375+OleksandrShulha@users.noreply.github.com>
2024-03-19 13:47:48 +0200

    Added Money Generatro

---
+++ DefvsMonstr/Assets/Scripts/MoneyGenerator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class MoneyGenerator : MonoBehaviour
+{
+    [SerializeField] private Image moneyDarkImage;
+    private RectTransform rt;
+    private int moneyOnLevel=0;
+    [SerializeField] private int deltaMoneyChange = 8;
+    [SerializeField] private TextMeshProUGUI moneyText;
+
+    private void Awake()
+    {
+        rt = moneyDarkImage.GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        MoneyOnStartLevel();
+    }
+
+
+
+    public void MoneyOnStartLevel()
+    {
+        moneyOnLevel = 0;
+        moneyText.text = moneyOnLevel + "$";
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, 0);
+        StartCoroutine(MoneyGenerate());   
+    }
+
+    IEnumerator MoneyGenerate()
+    {
+        while (rt.sizeDelta.y <= 150)
+        {
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, rt.sizeDelta.y + 1.5f);
+            yield return new WaitForSecondsRealtime(0.02f);
+        }
+        moneyOnLevel += deltaMoneyChange;
+        moneyText.text = moneyOnLevel + "$";
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, 0);
+        StartCoroutine(MoneyGenerate());
+    }
+
+    public void SetDeltaMoneyChange(int newDeltaMoney)
+    {
+        deltaMoneyChange = newDeltaMoney;
+    }
+
+    public int GetDeltaMoneyChange()
+    {
+        return deltaMoneyChange;
+    }
+
+}
